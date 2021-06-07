e3a4c1a4f9c32573573b1149151887a76b6fc103
RasikaGawande <56964294+RasikaGawande@users.noreply.github.com>
2021-06-07 22:36:20 +0530

    Update HelloController.java

--- src/main/java/com/db/internship/microservice1/controller/HelloController.java
+++ src/main/java/com/db/internship/microservice1/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String getMessage() {
-        return "Hello! from microservice RASIKA";
+        return "Hello! from microservice-1";
     }
 }
